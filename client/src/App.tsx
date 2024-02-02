c4d3deff522a58261597082011feb41a6b8d180f
LukasMaselsky <lukasmaselsky@gmail.com>
2024-02-02 15:52:01 +0000

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="flex bg-red-500 px-8 text-red-800 ">Hello</div>;
+}
+
+export default App;
---
+++ client/src/components/Calculator/Calculator.tsx
@@ -0,0 +1,8 @@
+export default function Calculator() {
+  return (
+    <div className="flex flex-col ">
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
